32795541ec63b70502598623b639640c167ff2c5
QuentinPace <quentinepace@gmail.com>
2024-11-07 12:28:57 -0500

    hidden user button

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import { useSelector } from "react-redux";
 
 function Navigation() {
+  const sessionUser = useSelector((state) => state.session.user);
+  const profileButtonClassName = sessionUser ? "" : "hidden-profile-button"
+  console.log(profileButtonClassName)
+
+
   return (
     <ul>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
 
-      <li>
-        <ProfileButton />
+      <li className={profileButtonClassName}>
+        <ProfileButton/>
       </li>
     </ul>
   );
