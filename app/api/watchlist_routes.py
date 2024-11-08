3cfd426846e45ebf2acc7ba9ac65ec1ddc8c41f0
Tingyang Zhou <zty_312@163.com>
2024-11-07 23:47:43 -0800

    get current user watchlist

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -1,14 +1,19 @@
-from flask import Blueprint, request
-from flask_login import login_required
+from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user
+
 
 # from app.api.aws import get_unique_filename, upload_file_to_s3
 # from app.forms import TweetForm
-from app.models import Stock, db
+from app.models import Stock, db, WatchlistStock
 
 watchlist_routes = Blueprint("watchlist", __name__)
 
 
-@watchlist_routes.route("/")
+@watchlist_routes.route("/current")
 @login_required
 def stocks():
-    return 
+    # Using filter with a condition
+    watchlist = WatchlistStock.query.filter(WatchlistStock.user_id == current_user.id).all()
+    watchlist_data = [item.to_dict() for item in watchlist]
+    return jsonify({"watchlist_stocks": watchlist_data}), 200
+
--- app/models/watchlistStock.py
+++ app/models/watchlistStock.py
@@ -3,9 +3,6 @@ from .db import SCHEMA, add_prefix_for_prod, db, environment
 class WatchlistStock(db.Model):
     __tablename__ = "watchlist_stocks"
 
-
-#     __tablename__ = "tweets"
-
     if environment == "production":
         __table_args__ = {"schema": SCHEMA}
     
@@ -13,5 +10,29 @@ class WatchlistStock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete="CASCADE"), nullable=False)
     stock_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('stocks.id'), ondelete="CASCADE"), nullable=False)
+
+
+
+
+    def to_dict_basic(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "stock_id": self.stock_id
+
+        }
+
+    def to_dict(self):
+        return {
+            
+            "id": self.id,
+            "ticker": self.stocks.to_dict_basic()['ticker'],
+            "company_name": self.stocks.to_dict_basic()['company_name'],
+            "image_url": self.stocks.to_dict_basic()['image_url'],
+            "company_info": self.stocks.to_dict_basic()['company_info'],
+            "updated_price": self.stocks.to_dict_basic()['updated_price']
+        }
+
+
     
     
