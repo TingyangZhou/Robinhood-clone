5726988eb60e397f5e1632786119662ae70be543
QuentinPace <quentinepace@gmail.com>
2024-11-08 16:52:06 -0500

    added /api prefix on a couple auth routes

--- react-vite/src/redux/session.js
+++ react-vite/src/redux/session.js
@@ -11,7 +11,7 @@ const removeUser = () => ({
 });
 
 export const thunkAuthenticate = () => async (dispatch) => {
-	const response = await fetch("/auth/");
+	const response = await fetch("api/auth/");
 	if (response.ok) {
 		const data = await response.json();
 		if (data.errors) {
@@ -23,7 +23,7 @@ export const thunkAuthenticate = () => async (dispatch) => {
 };
 
 export const thunkLogin = (credentials) => async dispatch => {
-  const response = await fetch("/auth/login", {
+  const response = await fetch("api/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(credentials)
