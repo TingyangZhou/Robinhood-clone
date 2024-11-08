b735fdddb073c66ad8aca9d0d7bc1acc4b01ef26
Tingyang Zhou <zty_312@163.com>
2024-11-08 08:29:26 -0800

    load_watchlist

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -8,12 +8,19 @@ from app.models import Stock, db, WatchlistStock
 
 watchlist_routes = Blueprint("watchlist", __name__)
 
-
+#Get Current User's Watchlisted Stocks
 @watchlist_routes.route("/current")
 @login_required
 def stocks():
-    # Using filter with a condition
     watchlist = WatchlistStock.query.filter(WatchlistStock.user_id == current_user.id).all()
     watchlist_data = [item.to_dict() for item in watchlist]
     return jsonify({"watchlist_stocks": watchlist_data}), 200
+
+
+#Add a Stock to Current User's Watchlist
+@watchlist_routes.route("/<int:stockId>/current")
+@login_required
+def add_stock(stockId):
+    stock = WatchlistStock(current_user.id, stockId)
+    current_user.watchlist_stocks
 
