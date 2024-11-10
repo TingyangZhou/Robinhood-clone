cc6129f0a071265329c46089e4961111764695cc
Adrian-gh1 <Adrian22046@gmail.com>
2024-11-10 00:16:49 -0800

    RemoveFromPortfolioThunk

--- react-vite/src/redux/portfolio.js
+++ react-vite/src/redux/portfolio.js
@@ -1,58 +1,79 @@
 // app/api/portfolio.js
 
-const GET_PORTFOLIO = 'portfolio/getPortfolio';
-const CREATE_PORTFOLIO = 'portfolio/createPortfolio';
+const GET_USER_STOCKS = 'portfolio/getUserStocks';
+const ADD_USER_STOCK = 'portfolio/addUserStock';
+const REMOVE_USER_STOCK = 'portfolio/removeUserStock';
 const initialState = {
-    portfolio: {}
+    userStocks: []
 };
 
 // Action Creators
-export const getPortfolioAction = (portfolio) => {
+export const getUserStocksAction = (userStocks) => {
     return {
-        type: GET_PORTFOLIO,
-        portfolio
+        type: GET_USER_STOCKS,
+        userStocks
     };
 };
 
-export const createPortfolioAction = (portfolio) => {
+export const addUserStockAction = (stock) => {
     return {
-        type: CREATE_PORTFOLIO,
-        portfolio
+        type: ADD_USER_STOCK,
+        stock
+    };
+};
+
+export const removeUserStockAction = (stockId) => {
+    return {
+        type: REMOVE_USER_STOCK,
+        stockId
     };
 };
 
 // Thunk Actions
-export const getAllPortfolioThunk = () => async (dispatch) => {
-    const response = await fetch('/api/portfolio', {
+export const getUserStocksThunk = () => async (dispatch) => {
+    const response = await fetch('/api/portfolio/current', {
         method: 'GET'
     });
     const data = await response.json();
-    dispatch(getPortfolioAction(data.portfolio));
+    dispatch(getUserStocksAction(data.portfolio_stocks));
     return response;
 };
 
-export const createPortfolioStockThunk = (portfolioData) => async (dispatch) => {
-    const response = await fetch('/api/portfolio', {
+export const addUserStockThunk = (stockId, stockData) => async (dispatch) => {
+    const response = await fetch(`/api/portfolio/${stockId}/current`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(portfolioData)
+        body: JSON.stringify(stockData)
     });
     const data = await response.json();
-    dispatch(createPortfolioAction(data));
+    dispatch(addUserStockAction(data.stock));
     return data;
 };
+
+export const removeUserStockThunk = (stockId) => async (dispatch) => {
+    const response = await fetch(`/api/portfolio/${stockId}/current`, {
+        method: 'DELETE'
+    });
+    const data = await response.json();
+    dispatch(removeUserStockAction(stockId));
+    return data;
+};
+
+
 
 // Reducer
 const portfolioReducer = (state = initialState, action) => {
     switch (action.type) {
-        case GET_PORTFOLIO:
-            return {...state, portfolio: action.portfolio};
-        case CREATE_PORTFOLIO:
-            return {...state, portfolio: action.portfolio};
+        case GET_USER_STOCKS:
+            return {...state, userStocks: action.userStocks};
+        case ADD_USER_STOCK:
+            return {...state, userStocks: [...state.userStocks, action.stock]};
+        case REMOVE_USER_STOCK:
+            return {...state, userStocks: state.userStocks.filter(stock => stock.id !== action.stockId)};
         default:
-            return state; 
+            return state;
     }
 };
 
