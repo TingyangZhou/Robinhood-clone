6f0e70b92fa5f635c8ff904e07a5c4a8b6493330
QuentinPace <quentinepace@gmail.com>
2024-11-11 17:50:42 -0500

    stop propogation on stock list items

--- react-vite/src/components/AllStocksList/AllStocksList.jsx
+++ react-vite/src/components/AllStocksList/AllStocksList.jsx
@@ -24,7 +24,10 @@ export default function AllStocksList({stocks, pageSize, heightPx}) {
                     <div className="company-name-list-item"><p>{arrStocks[i].company_name.length > 24 ? arrStocks[i].company_name.substring(0, 23) + "...": arrStocks[i].company_name}</p></div>
                     <div className="ticker-list-item"><p>{arrStocks[i].ticker}</p></div>
                     <div className="updated-price-list-item"><p>${arrStocks[i].updated_price}</p></div>
-                    <div className="button-list-item"><button onClick={() => dispatch(addToWatchlistThunk(arrStocks[i].id))}>{arrStocks[i].is_in_watchlist ? "-" : "+"}</button></div>
+                    <div className="button-list-item"><button onClick={(e) => {
+                        e.stopPropagation()
+                        dispatch(addToWatchlistThunk(arrStocks[i].id))
+                        }}>{arrStocks[i].is_in_watchlist ? "-" : "+"}</button></div>
                 </div>
             ))
         }
