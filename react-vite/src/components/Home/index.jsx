6369e87db61e257eab80c131c0fefc18598e4364
QuentinPace <quentinepace@gmail.com>
2024-11-12 11:44:42 -0500

    input my code from last night that didnt merge succesfully

--- react-vite/src/components/Home/index.jsx
+++ react-vite/src/components/Home/index.jsx
@@ -7,7 +7,9 @@ import AllStocksList from '../AllStocksList';
 import WatchlistStocksList from '../WatchlistStocksList';
 import "./indexHome.css"
 
+
 import { getAllWatchlistThunk, addToWatchlistThunk, removeFromWatchlistThunk } from '../../redux/watchlist';
+
 
 export default function Home() {
     // const data = useLoaderData();
@@ -15,15 +17,19 @@ export default function Home() {
     const sessionUser = useSelector((state) => state.session.user);
     const allStocks = useSelector((state) => state.stocks.stocks);
 
+
     // useEffect(() => {
     //     dispatch(getAllStocksThunk())
     // }, [dispatch])
+
+
 
 
     useEffect(() => {
         dispatch(getAllWatchlistThunk())
         dispatch(getAllStocksThunk())
     }, [dispatch])
+
 
     // useEffect(() => {
     //     async function testDeleteToWatchlist() {
@@ -37,14 +43,18 @@ export default function Home() {
     // }, [dispatch]);
 
 
+
+
     if (!sessionUser) {
         return <Navigate to='/login'></Navigate>
     }
 
+
     return (
         <main>
-            {Object.keys(allStocks).length && <AllStocksList stocks={allStocks} pageSize={12} heightPx={675}/>}
+            <AllStocksList stocks={allStocks} pageSize={12} heightPx={675}/>
             <WatchlistStocksList/>
         </main>
     );
 }
+
--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,26 +1,80 @@
-import { NavLink } from "react-router-dom";
-import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getAllSearchStocksThunk, getAllStocksThunk } from "../../redux/stocks";
+import { useDispatch } from "react-redux";
+import { useNavigate, NavLink } from "react-router-dom";
+import ProfileButton from "./ProfileButton.jsx"
+
+
+
 
 
 
 function Navigation() {
   const sessionUser = useSelector((state) => state.session.user);
-  const profileButtonClassName = sessionUser ? "" : "hidden-profile-button"
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  // const location = useLocation()
 
-  const homeClassname = sessionUser ? "": "hidden-home-link"
+
+  const [searchInput, setSearchInput] = useState("");
+
+
+  // useEffect(() => {
+  //   setSearchInput("")
+
+
+  // }, [location])
+
+
+
+
+
+
+  const handleSearch = async () => {
+    navigate("")
+    if(searchInput == ""){
+      dispatch(getAllStocksThunk())
+    }
+    else{
+      dispatch(getAllSearchStocksThunk(searchInput))
+    }
+    console.log("Search submitted:", searchInput);
+  };
+
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSearch();
+    }
+  };
+
+
+
+
+  const navClassName = sessionUser ? "nav-bar-main": "hidden-home-link"
   return (
-    <ul>
-      <li className={homeClassname}>
+    <header  className={navClassName}>
+      <div>
         <NavLink to="/">Home</NavLink>
-      </li>
-
-      <li className={profileButtonClassName}>
+      </div>
+      <div><input
+       type="text"
+       placeholder="search for stocks..."
+       value={searchInput}
+       onChange={(e) => setSearchInput(e.target.value)}
+       onKeyDown={handleKeyDown}
+       ></input></div>
+      <div>
         <ProfileButton/>
-      </li>
-    </ul>
+      </div>
+    </header>
   );
 }
 
+
 export default Navigation;
+
+
